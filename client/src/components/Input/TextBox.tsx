12a8793a5a356bcf19d2f0818cc9a343803dde4e
bohyeon <shannon_@hotmail.co.kr>
2021-10-11 20:46:26 +0900

    ✨ textbox component 추가

---
+++ client/src/components/Input/TextBox.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+export interface ITextProps extends React.InputHTMLAttributes<HTMLInputElement> {
+    width?: string;
+}
+
+const TextBox = ({ width, type = 'text', ...rest }: ITextProps) => {
+    return <CustomInput type={type} width={width} {...rest} autoComplete="false" />;
+};
+
+const CustomInput = styled.input<ITextProps>`
+    width: ${props => (props.width ? props.width : '100')}%;
+    padding: 0.5em;
+    margin: 0.5em;
+    box-sizing: border-box;
+    border: 1px solid ${props => props.theme.colors.black};
+    background: none;
+`;
+
+export default TextBox;
