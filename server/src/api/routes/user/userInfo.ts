2c1df77b0f4b93840f99730604eb44c4ec753002
bohyeon <shannon_@hotmail.co.kr>
2021-10-12 18:11:23 +0900

    ✅ 소환사 검색 페이지 테스트
    - (서버 단)리그 가져오기 성공

--- server/src/api/routes/user/userInfo.ts
+++ server/src/api/routes/user/userInfo.ts
@@ -1,15 +1,53 @@
-import axios from 'axios';
-import express, { Request, Response, NextFunction } from 'express';
+import { AxiosResponse } from 'axios';
+import express, { Request, Response } from 'express';
 import reqSender from '../../../utils/reqSender';
 
 const router = express.Router();
 
-router.get('/getSummonerId/:name', async (req: Request, res: Response, next: NextFunction) => {
-    const abd = await reqSender({
-        url: `/lol/summoner/v4/summoners/by-name/${encodeURIComponent(req.params.name)}`,
+interface ISummonerInfo {
+    id: string;
+    accountId: string;
+    puuid: string;
+    name: string;
+    profileIconId: number;
+    revisionDate: number;
+    summonerLevel: number;
+}
+
+interface ISummonerLeague {
+    leagueId: string;
+    summonerId: string;
+    summonerName: string;
+    queueType: string;
+    tier: string;
+    rank: string;
+    leaguePoints: number;
+    wins: number;
+    losses: number;
+    hotStreak: boolean;
+    veteran: boolean;
+    freshBlood: boolean;
+    inactive: boolean;
+}
+
+const getSummonerInfoByName = async (name: string): Promise<string> => {
+    const { data: summonerInfo }: AxiosResponse<ISummonerInfo> = await reqSender({
+        url: `/lol/summoner/v4/summoners/by-name/${encodeURIComponent(name)}`,
         method: 'GET',
     });
-    res.send(abd);
+
+    return summonerInfo.id;
+};
+
+router.get('/getSummonerId/:name', async (req: Request, res: Response) => {
+    getSummonerInfoByName(req.params.name).then(async (id: string) => {
+        const { data: leagues }: AxiosResponse<ISummonerLeague> = await reqSender({
+            url: `/lol/league/v4/entries/by-summoner/${id}`,
+            method: 'GET',
+        });
+
+        res.send(leagues);
+    });
 });
 
 export = router;
